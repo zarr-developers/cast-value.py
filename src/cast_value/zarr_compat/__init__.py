fa7e6f7a64ba575cd5e7e44bd4b832d38cd71c39
Davis Vann Bennett <davis.v.bennett@gmail.com>
2026-04-01 16:22:50 +0200

    refactor: simplify top-level exports

--- src/cast_value/zarr_compat/__init__.py
+++ src/cast_value/zarr_compat/__init__.py
@@ -1,14 +1,5 @@
 from __future__ import annotations
 
-try:
-    import zarr  # noqa: F401
-except ImportError as e:
-    _MSG = (
-        "The 'zarr' package is required to use cast_value.zarr_compat. "
-        "Install it with: pip install 'cast-value[zarr]'"
-    )
-    raise ImportError(_MSG) from e
-
 from cast_value.zarr_compat.v1 import CastValueNumpyV1, CastValueRustV1
 
 __all__ = ["CastValueNumpyV1", "CastValueRustV1"]
